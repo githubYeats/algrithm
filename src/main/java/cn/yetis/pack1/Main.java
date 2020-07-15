a22dfc55b541695b3330e9c875b9814fe20813a4
fyyang <fyyang@wisedu.com>
2020-07-15 19:30:20 +0800

    求两个List的交合并补

--- src/main/java/cn/yetis/pack1/Main.java
+++ src/main/java/cn/yetis/pack1/Main.java
@@ -17,11 +17,28 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println(InetAddress.getLocalHost().getHostAddress());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
+        // 注意：一定要使用创建对象的格式创建数组
+        Integer[] a = new Integer[]{6, 3, 9, 3, 2, 4, 5, 7};
+        Integer[] b = new Integer[]{5, 8, 6, 2, 1, 9};
+        List _a = Arrays.asList(a);
+        List _b = Arrays.asList(b);
+// 创建集合
+        Collection realA = new ArrayList<Integer>(_a);
+        Collection realB = new ArrayList<Integer>(_b);
+// 求交集
+        realA.retainAll(realB);
+        System.out.println("交集结果：" + realA);
+        Set result = new HashSet();
+// 求全集
+        result.addAll(_a);
+        result.addAll(_b);
+        System.out.println("全集结果：" + result);
+// 求差集：结果
+        Collection aa = new ArrayList(realA);
+        Collection bb = new ArrayList(result);
+        bb.removeAll(aa);
+        System.out.println("最终结果：" + bb);
+
 
     }
 
