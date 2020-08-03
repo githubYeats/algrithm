ecb325edea1860209f4d20ff4a780c37836c6b00
fyyang <fyyang@wisedu.com>
2020-08-03 17:54:09 +0800

    空Map取值，返回null，不报错；空对象取字段值，返回null，不报错

--- src/main/java/cn/yetis/pack1/Employee.java
+++ src/main/java/cn/yetis/pack1/Employee.java
@@ -1,10 +1,15 @@
 package cn.yetis.pack1;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author fyyang
  * @date
  * @since v1.0.0
  */
+@Data
+@NoArgsConstructor
 public class Employee {
 
     private String name;
--- src/main/java/cn/yetis/pack1/Main.java
+++ src/main/java/cn/yetis/pack1/Main.java
@@ -1,15 +1,8 @@
 package cn.yetis.pack1;
 
 
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.ArrayUtils;
-
-import java.lang.reflect.Field;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.time.ZoneId;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author fyyang
@@ -18,96 +11,15 @@ import java.util.stream.Collectors;
  */
 public class Main {
     public static void main(String[] args) {
-        // 注意：一定要使用创建对象的格式创建数组
-        Integer[] a = new Integer[]{6, 6, 3, 9, 3, 2, 4, 5, 7};
-        Integer[] b = new Integer[]{5, 8, 6, 2, 1, 9};
-        List _a = Arrays.asList(a);
-        List _b = Arrays.asList(b);
-// 创建集合
-        Collection realA = new ArrayList<Integer>(_a);
-        Collection realB = new ArrayList<Integer>(_b);
-// 求交集
-        realA.retainAll(realB);
-        System.out.println("交集结果：" + realA);
-        Set result = new HashSet();
-// 求全集
-        result.addAll(_a);
-        result.addAll(_b);
-        System.out.println("全集结果：" + result);
-// 求差集：结果
-        Collection aa = new ArrayList(realA);
-        Collection bb = new ArrayList(result);
-        bb.removeAll(aa);
-        System.out.println("最终结果：" + bb);
+        // 空 map
+        Map<String, String> emptyMap = new HashMap<>(0);
+        String s = emptyMap.get("1");
+        System.out.println(s);
 
-
-        System.out.println("=====================================");
-        // 求交集
-        Integer[] aaa = new Integer[]{6, 3, 9, 9, 3, 2, 4, 5, 7};
-        Integer[] bbb = new Integer[]{5, 8, 6, 2, 1, 9};
-        List<Integer> retainAAA = Arrays.asList(aaa);
-        List<Integer> retainBBB = Arrays.asList(bbb);
-        ArrayList<Integer> r_AAA = retainAAA.stream()
-                .collect(Collectors.toCollection(ArrayList::new));
-        ArrayList<Integer> r_BBB = retainBBB.stream()
-                .collect(Collectors.toCollection(ArrayList::new));
-
-        r_BBB.retainAll(r_AAA);
-        System.out.println(r_BBB);
-        System.out.println("=====================================");
-
-        r_AAA.retainAll(r_BBB);
-        System.out.println(r_AAA);
-        System.out.println("=====================================");
-        retainAAA.retainAll(retainBBB);
-        System.out.println(retainAAA);
-
-
+        // 空对象
+        Employee employee = new Employee();
+        String name = employee.getName();
+        System.out.println(name);
     }
 
-    public static List<String> getFiledName(Object obj) {
-        Field[] fields = obj.getClass().getDeclaredFields();
-        List<String> fieldNames = new ArrayList<>();
-        List<String> fieldValues = new ArrayList<>();
-        for (Field field : fields) {
-            fieldNames.add(field.getName());
-        }
-        return fieldNames;
-    }
-
-
-    /**
-     * org.apache.commons.collections4.CollectionUtils
-     */
-    public static void testCollectionUtils() {
-        String[] arrayA = new String[]{"1", "2", "3", "3", "4", "5"};
-        String[] arrayB = new String[]{"3", "4", "4", "5", "6", "7"};
-        List<String> a = Arrays.asList(arrayA);
-        List<String> b = Arrays.asList(arrayB);
-        //并集
-        Collection<String> union = CollectionUtils.union(a, b);
-        //交集
-        Collection<String> intersection = CollectionUtils.intersection(a, b);
-        //交集的补集
-        Collection<String> disjunction = CollectionUtils.disjunction(a, b);
-        //集合相减
-        Collection<String> subtract = CollectionUtils.subtract(a, b);
-        Collections.sort((List<String>) union);
-        Collections.sort((List<String>) intersection);
-        Collections.sort((List<String>) disjunction);
-        Collections.sort((List<String>) subtract);
-        System.out.println("A: " + ArrayUtils.toString(a.toArray()));
-        System.out.println("B: " + ArrayUtils.toString(b.toArray()));
-        System.out.println("--------------------------------------------");
-        System.out.println("Union(A, B): " + ArrayUtils.toString(union.toArray()));
-        System.out.println("Intersection(A, B): " + ArrayUtils.toString(intersection.toArray()));
-        System.out.println("Disjunction(A, B): " + ArrayUtils.toString(disjunction.toArray()));
-        System.out.println("Subtract(A, B): " + ArrayUtils.toString(subtract.toArray()));
-
-        System.out.println("--------------------------------------------");
-        String[] arrayC = new String[]{"1", "2", "3", "3", "3", "3", "4", "4", "4", "5"};
-        List<String> c = Arrays.asList(arrayC);
-        Collection<String> disjunctionC = CollectionUtils.disjunction(c, new HashSet<String>(c));
-        System.out.println("*******): " + ArrayUtils.toString(disjunctionC.toArray()));
-    }
 }
