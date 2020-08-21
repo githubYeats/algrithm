7ff535e81ebd71541bfe36a004037a4a48a22894
fyyang <fyyang@wisedu.com>
2020-08-21 17:31:48 +0800

    数字转"中文序号"

--- src/main/java/cn/yetis/pack1/Main.java
+++ src/main/java/cn/yetis/pack1/Main.java
@@ -1,8 +1,6 @@
 package cn.yetis.pack1;
 
 
-import java.util.Arrays;
-
 /**
  * @author fyyang
  * @date
@@ -10,31 +8,69 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        char arr[] = new char[52];
-        int i;
-        for (i = 0; i < 52; i++) {
-            if (i < 26) {
-                arr[i] = (char) ('A' + i);
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i + "==>" + getChineseOrder(i));
+        }
+        System.out.println(getChineseOrder(99999));
+
+    }
+
+    /**
+     * 根据所给数字，获取对应的中文序号
+     *
+     * @param i 数字，要求的范围：[0, 100000]
+     * @return 中文序号
+     */
+    private static String getChineseOrder(int i) {
+        if (i < 0 || i > 99999) {
+            throw new IllegalArgumentException("数字超出范围，需在[0, 99999]之内！");
+        }
+        String str = String.valueOf(i);
+        String[] s1 = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"};
+        String[] s2 = {"十", "百", "千", "万"};
+        //String[] s2 = {"十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千"};
+        StringBuilder result = new StringBuilder();
+        int n = str.length();
+        for (int j = 0; j < n; j++) {
+            int num = str.charAt(j) - '0';
+            if (j != n - 1 && num != 0) {
+                result.append(s1[num]).append(s2[n - 2 - j]);
             } else {
-                arr[i] = (char) ('a' + (i - 26));
+                result.append(s1[num]);
             }
         }
+        return wipeOutBeginOne(i, wipeOutEndZero(result.toString()));
+    }
 
-        int[] number = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-//        for (char c : arr) {
-//            for (int j : number) {
-//                System.out.println(c + String.valueOf(j));
-//            }
-//        }
-
-        for (int j : number) {
-            for (char c : arr) {
-                System.out.println(String.valueOf(j) + c);
-            }
+    /**
+     * 10~19，去除前端的"一"：10==>一十， 13==>一十三，19==>一十九
+     *
+     * @param i   所给数字
+     * @param str 中文序号
+     * @return 结果
+     */
+    private static String wipeOutBeginOne(int i, String str) {
+        int length = str.length();
+        if (i >= 10 && i <= 19 & str.startsWith("一")) {
+            str = str.substring(1, length);
         }
+        return str;
+    }
 
-
+    /**
+     * 去除末尾"零"（数字0本身转化出的"零"除外）：100==>一百零零，9900==>九千九百零零
+     *
+     * @param str 中文序号
+     * @return 结果
+     */
+    private static String wipeOutEndZero(String str) {
+        int length = str.length();
+        if (length > 2 && str.endsWith("零")) {
+            str = str.substring(0, length - 1);
+            // 针对"900 : 九百零零"这类情况，递归去除末尾"零"。
+            str = wipeOutEndZero(str);
+        }
+        return str;
     }
 
 }
