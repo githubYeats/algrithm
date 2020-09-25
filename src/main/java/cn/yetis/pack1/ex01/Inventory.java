50ef6ab45fd245cc46f54402ffe4440006be111e
fyyang <fyyang@wisedu.com>
2020-09-26 00:19:26 +0800

    卖吉他-04: 引入 GuitarSpec 类，改进程序设计。

--- src/main/java/cn/yetis/pack1/ex01/Inventory.java
+++ src/main/java/cn/yetis/pack1/ex01/Inventory.java
@@ -37,21 +37,23 @@ public class Inventory {
         for (Guitar guitar : guitars) {
             // serialNumber、price两者不判断了，因为它们都是唯一的。  price假设也是唯一的。
 
+            GuitarSpec guitarSpec = guitar.getGuitarSpec();
+
             String model = spec.getModel();
-            if ((!"".equals(model)) && (!model.equals(guitar.getGuitarSpec().getModel()))) {
+            if ((!"".equals(model)) && (!model.equals(guitarSpec.getModel()))) {
                 continue;
             }
 
-            if (!spec.getBuilder().equals(guitar.getGuitarSpec().getBuilder())) {
+            if (!spec.getBuilder().equals(guitarSpec.getBuilder())) {
                 continue;
             }
-            if (!spec.getType().equals(guitar.getGuitarSpec().getType())) {
+            if (!spec.getType().equals(guitarSpec.getType())) {
                 continue;
             }
-            if (!spec.getBackWood().equals(guitar.getGuitarSpec().getBackWood())) {
+            if (!spec.getBackWood().equals(guitarSpec.getBackWood())) {
                 continue;
             }
-            if (!spec.getTopWood().equals(guitar.getGuitarSpec().getTopWood())) {
+            if (!spec.getTopWood().equals(guitarSpec.getTopWood())) {
                 continue;
             }
 
